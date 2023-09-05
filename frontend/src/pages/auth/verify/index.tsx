218cc78351a146fa972f6320e1ec62c77807dcdc
Tilto_ <tilto0822@gmail.com>
2023-09-05 20:02:24 +0900

    fix: get search param

--- frontend/src/pages/auth/verify/index.tsx
+++ frontend/src/pages/auth/verify/index.tsx
@@ -1,11 +1,15 @@
 import { useEffect } from 'react';
 import { useAlert } from 'react-alert';
-import { useNavigate, useParams } from 'react-router-dom';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 export function VerifyPage() {
   const alert = useAlert();
   const navigate = useNavigate();
-  const { type, email, code } = useParams();
+  const [params] = useSearchParams();
+
+  const type = params.get('type');
+  const code = params.get('code');
+  const email = params.get('email');
 
   useEffect(() => {
     if (!code) {
@@ -19,7 +23,7 @@ export function VerifyPage() {
         alert.error('잘못된 접근입니다.');
         return navigate('/');
     }
-  }, [type, code]);
+  }, [params]);
 
   return <div>register with {email}</div>;
 }
