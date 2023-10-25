99831a0dbab2c2815220e5c34d803225ca2dccc3
seonwoo0808 <seonwoo0808@pentag.kr>
2023-10-25 14:20:33 +0900

    feat: add project create endpoint

---
+++ backend/routers/project.router.go
@@ -0,0 +1,22 @@
+package routers
+
+import (
+	jwtware "github.com/gofiber/contrib/jwt"
+	"github.com/gofiber/fiber/v2"
+	"implude.kr/VOAH-Backend-Core/configs"
+	"implude.kr/VOAH-Backend-Core/controllers/project"
+	"implude.kr/VOAH-Backend-Core/middleware"
+)
+
+func addProject(router *fiber.App) {
+	projectGroup := router.Group("/api/project")
+	projectGroup.Use(
+		jwtware.New(jwtware.Config{
+			SigningKey: jwtware.SigningKey{Key: configs.Env.Auth.JWTSecret},
+		}),
+		middleware.LastActivitMiddleware,
+	)
+	projectGroup.Post("/", func(c *fiber.Ctx) error {
+		return project.CreateProjectCtrl(c)
+	})
+}
