58ffd35afc62582ca5d7875a12b18e7222a23262
seonwoo0808 <seonwoo0808@pentag.kr>
2023-11-03 00:30:24 +0900

    chore: add expose on module

--- backend/models/module.go
+++ backend/models/module.go
@@ -5,6 +5,7 @@ import "time"
 type Module struct {
 	ID               int       `gorm:"primaryKey" json:"id"`
 	Enabled          bool      `gorm:"not null" json:"enabled"`
+	Expose           bool      `gorm:"not null" json:"expose"`
 	Version          string    `gorm:"size:20;not null" json:"version"`
 	Name             string    `gorm:"size:40;not null" json:"name"`
 	Description      string    `gorm:"size:300;not null" json:"description"`
